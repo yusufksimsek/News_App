782e7cba84b97293679357ce87a21f254fd65397
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-10-17 14:52:39 +0300

    bug fix: Compile and target sdk's updated to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.news_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.news_app"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
