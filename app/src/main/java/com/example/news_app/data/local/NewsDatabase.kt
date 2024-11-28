06e01e3fe072d895943dd44d620a849d89e5a41b
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-11-28 13:13:11 +0300

    Author made nullable in Article model and database version has changed

--- app/src/main/java/com/example/news_app/data/local/NewsDatabase.kt
+++ app/src/main/java/com/example/news_app/data/local/NewsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.news_app.domain.model.Article
 
-@Database(entities = [Article::class],version = 1,)
+@Database(entities = [Article::class],version = 2,)
 @TypeConverters(NewsTypeConvertor::class)
 abstract class NewsDatabase : RoomDatabase() {
     abstract val newsDao: NewsDao
